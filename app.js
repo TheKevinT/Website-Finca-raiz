5fc82d3c98e873654fb6cfb814dc6a0e04e64ed9
Kevin Tipantiza <39786491+TheKevinT@users.noreply.github.com>
2020-07-09 13:09:45 -0500

    crear proyecto

---
+++ app.js
@@ -0,0 +1,5 @@
+let options = document.getElementById("navbar-options")
+
+options.addEventListener("click" , ()=> {
+    document.getElementById("navbar-options-none").classList.toggle("navbar-options-none-2")
+})
